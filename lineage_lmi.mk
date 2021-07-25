68885da70dae81fc33f0e0748abafc3b8808b051
jackson177 <noor90saleh@gmail.com>
2021-07-25 20:56:32 +0000

    Rebase To ok google works and other stuff
    Signed-off-by: jackson177 <noor90saleh@gmail.com>

--- lineage_lmi.mk
+++ lineage_lmi.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Derp stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
@@ -20,7 +20,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
   ro.derp.cpu=sm8250 \
   ro.derp.rom=Derp
 
-PRODUCT_NAME := derp_lmi
+# Face Unlock
+TARGET_FACE_UNLOCK := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_FOD_ANIMATIONS := true
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+PRODUCT_NAME := lineage_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
